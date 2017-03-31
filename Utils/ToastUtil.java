3eb5289b4f9325c7907dd7ca6a29d832a31a365a
InnoFang <q984941707@qq.com>
2017-03-31 19:45:21 +0800

    update ToastUtil

--- Utils/ToastUtil.java
+++ Utils/ToastUtil.java
@@ -1,6 +1,9 @@
 
 import android.content.Context;
+import android.support.annotation.StringRes;
 import android.widget.Toast;
+
+// import io.innofang.myapplication.App; // 更改为自己的Application子类
 
 /**
  * Toast统一管理类
@@ -31,7 +34,7 @@ public class ToastUtil {
      * @param context
      * @param message
      */
-    public static void showShort(Context context, int message) {
+    public static void showShort(Context context, @StringRes int message) {
         if (isShow)
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
@@ -53,7 +56,7 @@ public class ToastUtil {
      * @param context
      * @param message
      */
-    public static void showLong(Context context, int message) {
+    public static void showLong(Context context, @StringRes int message) {
         if (isShow)
             Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
@@ -77,9 +80,45 @@ public class ToastUtil {
      * @param message
      * @param duration
      */
-    public static void show(Context context, int message, int duration) {
+    public static void show(Context context, @StringRes int message, int duration) {
         if (isShow)
             Toast.makeText(context, message, duration).show();
     }
 
+    private static Toast mToast;
+    private static long time = 0;
+
+    /**
+     * 显示Toast
+     * 点一次显示内容并且为短时间显示，若在两秒内点击两次，则更改为长时间显示
+     * 不会重复创建Toast
+     * <strong>App为Application子类</strong>
+     * @param content
+     */
+    public static void showToast(String content) {
+        long temp = System.currentTimeMillis();
+        if (mToast == null) {
+            mToast = Toast.makeText(App.getContext(), content, Toast.LENGTH_SHORT);
+        }
+        mToast.setText(content);
+        if (temp - time < 2000) {
+            mToast.setDuration(Toast.LENGTH_LONG);
+        }
+        time = temp;
+        mToast.show();
+    }
+
+
+    public static void showToast(@StringRes int resId) {
+        long temp = System.currentTimeMillis();
+        if (mToast == null) {
+            mToast = Toast.makeText(App.getContext(), resId, Toast.LENGTH_SHORT);
+        }
+        mToast.setText(resId);
+        if (temp - time < 2000) {
+            mToast.setDuration(Toast.LENGTH_LONG);
+        }
+        time = temp;
+        mToast.show();
+    }
 }
