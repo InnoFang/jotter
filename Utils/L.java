365449b431777cd4e98e1caab3e507f77648a28f
InnoFang <q984941707@qq.com>
2017-04-25 10:42:49 +0800

    update Utils

--- Utils/L.java
+++ Utils/L.java
@@ -1,11 +1,14 @@
-
+﻿
 import android.util.Log;
 
 /**
- * Log统一管理类
+ * Author: Inno Fang
+ * Description: Log 管理类
  */
+
+
 public class L {
-    private static final String TAG = "L";
+    private static final String TAG = "Log";
 
     private L() {
         /* cannot be instantiated */
@@ -13,47 +16,57 @@ public class L {
     }
 
     /* 是否需要打印bug，可以在application的onCreate函数里面初始化 */
-    public static boolean isDebug = true;
-
-    /* 下面四个是默认tag的函数 */
-    public static void i(String msg) {
-        if (isDebug)
-            Log.i(TAG, msg);
-    }
-
-    public static void d(String msg) {
-        if (isDebug)
-            Log.d(TAG, msg);
-    }
-
-    public static void e(String msg) {
-        if (isDebug)
-            Log.e(TAG, msg);
-    }
+    public static boolean sDebug = true;
 
     public static void v(String msg) {
-        if (isDebug)
+        if (sDebug)
             Log.v(TAG, msg);
     }
 
+    public static void d(String msg) {
+        if (sDebug)
+            Log.d(TAG, msg);
+    }
+
+    /* 下面四个是默认tag的函数 */
+    public static void i(String msg) {
+        if (sDebug)
+            Log.i(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        if (sDebug)
+            Log.e(TAG, msg);
+    }
+
+    public static void w(String msg) {
+        if (sDebug)
+            Log.w(TAG, msg);
+    }
+
     /* 下面是传入自定义tag的函数 */
-    public static void i(String tag, String msg) {
-        if (isDebug)
+    public static void v(String tag, String msg) {
+        if (sDebug)
             Log.i(tag, msg);
     }
 
     public static void d(String tag, String msg) {
-        if (isDebug)
+        if (sDebug)
             Log.i(tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (sDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (sDebug)
+            Log.w(tag, msg);
     }
 
     public static void e(String tag, String msg) {
-        if (isDebug)
-            Log.i(tag, msg);
-    }
-
-    public static void v(String tag, String msg) {
-        if (isDebug)
+        if (sDebug)
             Log.i(tag, msg);
     }
 }
